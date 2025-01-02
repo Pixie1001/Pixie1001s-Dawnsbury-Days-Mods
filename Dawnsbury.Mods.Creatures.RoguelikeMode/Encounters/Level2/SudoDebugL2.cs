982441e6c9d3276c9ce210168ec0309fec8230e9
Sudo <sudoprogramming@gmail.com>
2025-01-01 21:39:21 -0600

    - Added Nuglub - Added Temp Sudo Encounter

---
+++ Dawnsbury.Mods.Creatures.RoguelikeMode/Encounters/Level2/SudoDebugL2.cs
@@ -0,0 +1,8 @@
+﻿namespace Dawnsbury.Mods.Creatures.RoguelikeMode.Encounters.Level2
+{
+    [System.Runtime.Versioning.SupportedOSPlatform("windows")]
+    internal class SudoDebugL2 : Level2Encounter
+    {
+        public SudoDebugL2(string filename) : base("Sudo Debug", filename) { }
+    }
+}
