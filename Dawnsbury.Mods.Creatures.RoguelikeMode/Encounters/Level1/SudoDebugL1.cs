9ad31ccef6c1c318bbf0c7dceba52b357fb9744a
Sudo <sudoprogramming@gmail.com>
2025-01-01 21:32:14 -0600

    Revert "- Added Nuglub"
    This reverts commit 57cf6e32fb1a9c65e3a51777fdff7176910477e6.

--- Dawnsbury.Mods.Creatures.RoguelikeMode/Encounters/Level1/SudoDebugL1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dawnsbury.Mods.Creatures.RoguelikeMode.Encounters.Level1
-{
-    [System.Runtime.Versioning.SupportedOSPlatform("windows")]
-    internal class SudoDebugL1 : Level1Encounter
-    {
-        public SudoDebugL1(string filename) : base("Sudo Debug", filename) { }
-    }
-}
--- Dawnsbury.Mods.Creatures.RoguelikeMode/Encounters/Level3/SudoDebugL3.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dawnsbury.Mods.Creatures.RoguelikeMode.Encounters.Level3
-{
-    [System.Runtime.Versioning.SupportedOSPlatform("windows")]
-    internal class SudoDebugL3 : Level3Encounter
-    {
-        public SudoDebugL3(string filename) : base("Sudo Debug", filename) { }
-    }
-}
