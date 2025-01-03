32e797401481a1251296f38dc504880703b0fa22
Sudo <sudoprogramming@gmail.com>
2025-01-03 16:24:07 -0600

    Fixed Encounter Index

--- Dawnsbury.Mods.Creatures.RoguelikeMode/Tables/EncounterTables.cs
+++ Dawnsbury.Mods.Creatures.RoguelikeMode/Tables/EncounterTables.cs
@@ -54,8 +54,8 @@ namespace Dawnsbury.Mods.Creatures.RoguelikeMode.Tables {
             encounters[1].Add(new TypedEncounterCampaignStop<HideAndSeekLv2>());
             encounters[1].Add(new TypedEncounterCampaignStop<ShadowCasterSanctumLv2>());
             encounters[1].Add(new TypedEncounterCampaignStop<MysteriousRoom_FeyL2>());
-            encounters[0].Add(new TypedEncounterCampaignStop<MysteriousRoom_HomunculusL2>());
-            encounters[0].Add(new TypedEncounterCampaignStop<MysteriousRoom_ArtRoomL2>());
+            encounters[1].Add(new TypedEncounterCampaignStop<MysteriousRoom_HomunculusL2>());
+            encounters[1].Add(new TypedEncounterCampaignStop<MysteriousRoom_ArtRoomL2>());
 
             // level3Encounters
             encounters[2].Add(new TypedEncounterCampaignStop<DrowAmbushLv3>());
@@ -68,8 +68,8 @@ namespace Dawnsbury.Mods.Creatures.RoguelikeMode.Tables {
             encounters[2].Add(new TypedEncounterCampaignStop<HideAndSeekLv3>());
             encounters[2].Add(new TypedEncounterCampaignStop<ShadowCasterSanctumLv3>());
             encounters[2].Add(new TypedEncounterCampaignStop<MysteriousRoom_FeyL3>());
-            encounters[0].Add(new TypedEncounterCampaignStop<MysteriousRoom_HomunculusL3>());
-            encounters[0].Add(new TypedEncounterCampaignStop<MysteriousRoom_ArtRoomL3>());
+            encounters[2].Add(new TypedEncounterCampaignStop<MysteriousRoom_HomunculusL3>());
+            encounters[2].Add(new TypedEncounterCampaignStop<MysteriousRoom_ArtRoomL3>());
 
             // level1EliteEncounters
             eliteEncounters[0].Add(new TypedEncounterCampaignStop<HallOfSmokeLv1>());
