3829f79fc662c9ec340bcdc73c6325a1573b912d
JMarohl <jake_marohl@sbcglobal.net>
2025-03-05 20:48:06 -0700

    Remove unnecessary adjustments from colosseum 1

--- Dawnsbury.Mods.Creatures.RoguelikeMode/Encounters/Level3/Colosseum1Lv3.cs
+++ Dawnsbury.Mods.Creatures.RoguelikeMode/Encounters/Level3/Colosseum1Lv3.cs
@@ -28,7 +28,7 @@ namespace Dawnsbury.Mods.Creatures.RoguelikeMode.Encounters.Level3
 
                 for (int i = 0; i < 4; i++)
                 {
-                    var creature = Bodyguard.Create().ApplyEliteAdjustments(false);
+                    var creature = Bodyguard.Create();
                     creature.MainName = "Gladiator";
                     battle.SpawnCreature(creature, battle.Enemy, 6 + i % 2, 2 + ((i / 2) % 2));
                 }
@@ -48,14 +48,14 @@ namespace Dawnsbury.Mods.Creatures.RoguelikeMode.Encounters.Level3
 
                         for (int i = 0; i < 2; i++)
                         {
-                            var creature = Bodyguard.Create().ApplyEliteAdjustments(false);
+                            var creature = Bodyguard.Create();
                             creature.MainName = "Gladiator";
                             battle.SpawnCreature(creature, battle.Enemy, 6 + i % 2, 3);
                         }
 
                         for (int i = 0; i < 2; i++)
                         {
-                            battle.SpawnCreature(Pikeman.Create().ApplyEliteAdjustments(false), battle.Enemy, 6 + i % 2, 2);
+                            battle.SpawnCreature(Pikeman.Create(), battle.Enemy, 6 + i % 2, 2);
                         }
 
                         foreach (var character in battle.AllCreatures.Where((creature) => creature.OwningFaction == battle.You))
@@ -81,7 +81,7 @@ namespace Dawnsbury.Mods.Creatures.RoguelikeMode.Encounters.Level3
 
                         var champion = R.Coin() ? RalknarTheRude.Create() : SorantonTheSkilled.Create();
 
-                        battle.SpawnCreature(champion.ApplyEliteAdjustments(false), battle.Enemy, 7, 2);
+                        battle.SpawnCreature(champion, battle.Enemy, 7, 2);
 
                         foreach (var character in battle.AllCreatures.Where((creature) => creature.OwningFaction == battle.You))
                         {
